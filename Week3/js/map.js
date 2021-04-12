876a35bdca343d4ecda51a23c0897fa2d1152247
ec028 <echau028@g.ucla.edu>
2021-04-12 15:52:19 -0700

    Week 3 Lab Assignment

---
+++ Week3/js/map.js
@@ -0,0 +1,50 @@
+let books = [
+	    {
+	    'title':'Trinity College Library in Dublin, Ireland',
+	    'lat': 53.34401767044433,
+	    'lon': -6.256528282655274,
+	    'description': 'This library has the <b>Book of Kells</b>, the most famous of the illuminated manuscripts.',
+	    },
+        {
+        'title':'Bodelian Library in Oxford, United Kingdom',
+        'lat': 51.754278867517435, 
+		'lon': -1.253999086476985,
+		'description': 'This library can access a free copy of all books printed in Great Britain.',
+	    },
+	    {
+		'title':'St. Catherine\'s Monestary in Sinai, Egypt',
+		'lat': 28.556183974211535, 
+		'lon': 33.97591925137211,
+		'description': 'This monestary is actually the world\'s oldest continously operating library.',
+	    },
+	    {
+		'title':'The Library of El Escorial in Madrid, Spain',
+		'lat': 40.59707464472327, 
+		'lon': -4.1289940437718755,
+		'description': 'This library is located in a historical residence for the King of Spain.',
+	    },
+	    {
+		'title':'Strahov Library in Prague, Czech Republic',
+		'lat': 50.08653974620061,
+		'lon': 14.38862788276644,
+		'description': 'The ceilings of this library are adorned with beautiful Baroque-era artwork.',
+	    }
+	]
+	//establishing a set point on the map
+	var map = L.map('map').setView([53.34401767044433, -6.256528282655274], 2);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	//initial marker	
+	var marker = L.marker([53.34401767044433, -6.256528282655274]).addTo(map)
+		.bindPopup('Trinity College Library in Dublin, Ireland')
+		.openPopup();	
+
+	// loop through books to establish each point
+	books.forEach(function(item){
+		var marker = L.marker([item.lat, item.lon]).addTo(map)
+		.bindPopup(item.title + ":" +item.description)
+		.openPopup();	
+		});
